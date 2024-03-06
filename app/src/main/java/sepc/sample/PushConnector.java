55245283cf81cb17f74b1fcd50c09152c0a0a614
Bilal <bilalakhter.alphasquad@gmail.com>
2024-03-07 01:09:03 +0500

    fixes

--- app/src/main/java/sepc/sample/PushConnector.java
+++ app/src/main/java/sepc/sample/PushConnector.java
@@ -70,7 +70,7 @@ public class PushConnector {
                     isAdded = true;
                 }
                 try {
-                    Thread.sleep(20);
+                    Thread.sleep(5);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
