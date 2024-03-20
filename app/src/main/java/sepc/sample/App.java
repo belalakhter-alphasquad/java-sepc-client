2273afc8aca7044f53360a6dd3094fbb4846e53a
Bilal <bilalakhter.alphasquad@gmail.com>
2024-03-20 22:06:27 +0500

    changes

--- app/src/main/java/sepc/sample/App.java
+++ app/src/main/java/sepc/sample/App.java
@@ -14,10 +14,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class App {
+    private static final String USER = System.getProperty("DB_USER");
+
     public static void main(String[] args) {
         final Logger logger = LoggerFactory.getLogger(App.class);
 
         EnvLoader.load(".env");
+        logger.info("this is user " + USER);
         ShutdownSignalBarrier barrier = new ShutdownSignalBarrier();
 
         DbClient dbClient = new DbClient();
